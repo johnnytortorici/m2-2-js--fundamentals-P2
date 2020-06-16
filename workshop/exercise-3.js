0ff27bd9695fe8a92ed09de8a34f0c28f8f22b84
Johnny Tortorici <johnny@Johnnys-MacBook-Pro.local>
2020-06-16 17:03:09 -0400

    Exercise 3

--- workshop/exercise-3.js
+++ workshop/exercise-3.js
@@ -4,10 +4,26 @@
 
 // Also, wouldn't it be nice if there were a Math method that could simplify the code....
 // https://www.w3schools.com/jsreF/jsref_obj_math.asp
+function maxNum(num1, num2) {
+    return Math.max(num1, num2);
+}
+
+console.log(maxNum(36, 27));
 
 // Q3.2
 // Without changing the function at all,
 // Can you figure out how we could use it to determine the greater number between 4 integers?
 
+console.log(maxNum(maxNum(248, 321), maxNum(198, 322)));
+
 // Q3.3
 // Would this work with more integers?
+
+let numbers = [248, 321, 198, 322, 426, 511, 379, 488];
+let biggestNum = '';
+
+for (let i = 0; i < numbers.length-1; i++) {
+    biggestNum = maxNum(numbers[i], numbers[i+1]);
+}
+
+console.log(biggestNum);
